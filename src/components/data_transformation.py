bbbdcd4272a371651f8e557851112ee3cd3798c5
hrisikeshOfc <hrishikesh@ineuron.ai>
2023-04-19 18:13:53 +0530

    training code completed

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,8 +13,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
-from src.components.data_clustering import DataClustering
+from src.utils import save_object, read_schema_config_file
 import os
 
 
@@ -24,15 +23,54 @@ class DataTransformationConfig:
 
 
 class DataTransformation:
-    def __init__(self):
+    def __init__(self,
+                 train_file_path,
+                 test_file_path):
         self.data_transformation_config = DataTransformationConfig()
-        
 
-    def get_data_transformer_object(self):
+        self.train_file_path = train_file_path
+        self.test_file_path = test_file_path
+
+    
+        
+    def apply_outliers_capping(self,dataframe:pd.DataFrame):
+        """
+            Method Name :   apply_outliers_capping
+            Description :   This method reduces the outliers
+            
+            Output      :   a pd.DataFrame
+            On Failure  :   Write an exception log and then raise an exception
+            
+            Version     :   1.2
+            Revisions   :   moved setup to cloud
+        """
+
+
+        try:
+
+            outliers_columns = read_schema_config_file()['outlier_columns']
+
+            df = dataframe.copy()
+            for column in outliers_columns:
+
+                percentile25 = df[column].quantile(0.25)
+                percentile75 = df[column].quantile(0.75)
+                iqr = percentile75 - percentile25
+                upper_limit = percentile75 + 1.5 * iqr
+                lower_limit = percentile25 - 1.5 * iqr
+                df.loc[(df[column]>upper_limit), column]= upper_limit
+                df.loc[(df[column]<lower_limit), column]= lower_limit   
+            
+
+            return df
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+    def get_data_transformer_object(self) -> object:
         try:
             
-            # define custom function to replace 'NA' with np.nan
-            replace_na_with_nan = lambda X: np.where(X == 'na', np.nan, X)
 
             # define the steps for the preprocessor pipeline
             imputer_step = ('imputer', KNNImputer(n_neighbors=3, weights='uniform',missing_values=np.nan))
@@ -44,7 +82,6 @@ class DataTransformation:
                 scaler_step
                 ]
             )
-            
             return preprocessor
 
         except Exception as e:
@@ -52,47 +89,37 @@ class DataTransformation:
 
 
 
-    def initiate_data_transformation(self, dataframe_path: str):
+    def initiate_data_transformation(self, 
+                                     train_file_path: str,
+                                     test_file_path: str):
         try:
-            df = pd.read_csv(dataframe_path)
+            train_df = pd.read_csv(train_file_path)
+            test_df = pd.read_csv(test_file_path)
 
-           
+            target_column_name =  read_schema_config_file()['target_column']
+
             #training dataframe
-
-            clustering = DataClustering(dataframe= df)
-            
-            clustered_train_data_dir, clustered_test_data_dir, trained_clustering_model_file_path = clustering.initiate_clustering()
-
-            
-
-
-            
-
-
-
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
 
 
             #testing dataframe
             input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
-            target_feature_test_df = test_df[target_column_name].map(target_column_mapping)
+            target_feature_test_df = test_df[target_column_name]
+
+            preprocessor = self.get_data_transformer_object()
+
 
             transformed_input_train_feature = preprocessor.fit_transform(input_feature_train_df)
 
             transformed_input_test_feature =preprocessor.transform(input_feature_test_df)
 
-            smt = SMOTETomek(sampling_strategy="minority")
             
 
-            input_feature_train_final, target_feature_train_final = smt.fit_resample(
-                transformed_input_train_feature, target_feature_train_df
-            )
+           
 
-            input_feature_test_final, target_feature_test_final = smt.fit_resample(
-                transformed_input_test_feature, target_feature_test_df
-            )
-
-            train_arr = np.c_[input_feature_train_final, np.array(target_feature_train_final) ]
-            test_arr = np.c_[ input_feature_test_final, np.array(target_feature_test_final) ]
+            train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df) ]
+            test_arr = np.c_[ transformed_input_test_feature, np.array(target_feature_test_df) ]
 
             save_object(self.data_transformation_config.preprocessor_obj_file_path,
                         obj= preprocessor)
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,19 +2,19 @@ import os
 import sys
 from dataclasses import dataclass
 
-from sklearn.ensemble import (
-    AdaBoostClassifier,
-    GradientBoostingClassifier,
-    RandomForestClassifier,
-)
+from sklearn.pipeline import Pipeline
+
 from sklearn.metrics import r2_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from xgboost import XGBClassifier
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import evaluate_models, load_object, save_object, upload_file
+from src.utils import  load_object, save_object, upload_file
 
 
 @dataclass
@@ -55,39 +55,48 @@ class ModelTrainer:
                 test_array[:, -1],
             )
 
-            models = {
-                "Random Forest": RandomForestClassifier(),
-                "Decision Tree": DecisionTreeClassifier(),
-                "Gradient Boosting": GradientBoostingClassifier(),
-                "K-Neighbors Classifier": KNeighborsClassifier(),
-                "XGBClassifier": XGBClassifier(),
-                "AdaBoost Classifier": AdaBoostClassifier(),
-            }
+            models = [
+                        ('Linear Regression', LinearRegression()),
+                        ('Ridge Regression', Ridge()),
+                        ('Lasso Regression', Lasso()),
+                        ('Random Forest Regression', RandomForestRegressor()),
+                        ('Gradient Boosting Regression', GradientBoostingRegressor())
+                    ]
 
             logging.info(f"Extracting model config file path")
 
-            model_report: dict = evaluate_models(X=x_train, y=y_train, models=models)
+            preprocessor = load_object(file_path=preprocessor_path)
 
-            ## To get best model score from dict
-            best_model_score = max(sorted(model_report.values()))
+            pipelines = []
+            for name, model in models:
+                pipelines.append((name, Pipeline([('preprocessor', preprocessor), (name, model)])))
 
-            ## To get best model name from dict
+           
+           # Define the parameter grid for GridSearchCV
+            param_grid = {
+                'Linear Regression': {},
+                'Ridge Regression': {'alpha': [0.01, 0.1, 1, 10]},
+                'Lasso Regression': {'alpha': [0.01, 0.1, 1, 10]},
+                'Random Forest Regression': {'n_estimators': [50, 100, 200], 'max_depth': [None, 5, 10]},
+                'Gradient Boosting Regression': {'n_estimators': [50, 100, 200], 'max_depth': [None, 5, 10], 'learning_rate': [0.01, 0.1, 1]},
+                'Neural Network Regression': {'hidden_layer_sizes': [(50, 50), (100, 50, 25)], 'alpha': [0.0001, 0.001, 0.01]}
+            }
 
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
+            # Define the GridSearchCV object
+            grid = GridSearchCV(estimator=pipelines, param_grid=param_grid, scoring='neg_mean_squared_error', cv=5, n_jobs=-1)
 
-            best_model = models[best_model_name]
-
-            if best_model_score < 0.6:
-                raise Exception("No best model found")
+            # Fit the grid search object to the training data
+            grid.fit(x_train, y_train)
 
             logging.info(f"Best found model on both training and testing dataset")
 
-            preprocessing_obj = load_object(file_path=preprocessor_path)
+            best_model = grid.best_estimator_
+            y_pred = best_model.predict(x_test)
+
+            
 
             custom_model = CustomModel(
-                preprocessing_object=preprocessing_obj,
+                preprocessing_object=preprocessor,
                 trained_model_object=best_model,
             )
 
